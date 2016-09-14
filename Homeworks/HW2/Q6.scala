3eb947f074caf3c680c118b65a4b29ce672cf6bb
Mayukh Nair <nairmayukh@gmail.com>
2016-09-14 23:43:01 +0530

    Updated Q6

--- Homeworks/HW2/Q6.scala
+++ Homeworks/HW2/Q6.scala
@@ -1,9 +1,19 @@
 object Q6
 {
-	def quickSort(inputlist:List[Int]):List[Int]
-	inputlist match {
-		case hd::rest => pivotSort(list,hd) 
-		case Nil => Nil
+	def pivotSort(inputlist:List[Int], inputnum:Int):List[Int]=
+	{
+		inputlist match {
+			case head::rest if (head <= inputnum) => head::pivotSort(rest,inputnum)
+			case Nil => List(inputnum)
+			case head::rest if (head > inputnum) => pivotSort(rest,inputnum) ::: List(head)
+		}
+	}
+	def quickSort(inputlist:List[Int]):List[Int]=
+	{
+		inputlist match {
+			case hd::rest => pivotSort(inputlist,hd) 
+			case Nil => Nil
+		}
 	}	
 def main(args: Array[String]): Unit= 
 	{
